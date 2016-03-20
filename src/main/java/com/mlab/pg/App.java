98c5293797c3bfd0843c6f2569a139c27e273ac2
shiguera <shiguera@mercatorlab.com>
2016-03-20 09:44:05 +0100

    Initial commit

---
+++ src/main/java/com/mlab/pg/App.java
@@ -0,0 +1,13 @@
+package com.mlab.pg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
