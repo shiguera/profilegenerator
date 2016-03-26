eabde061283739d65529ebe7c9784fead7a044a2
shiguera <shiguera@mercatorlab.com>
2016-03-26 11:04:34 +0100

    Complete methods in XYVectorFunction except getCurvature()

--- src/main/java/com/mlab/pg/xyfunction/XYVectorFunction.java
+++ src/main/java/com/mlab/pg/xyfunction/XYVectorFunction.java
@@ -25,21 +25,43 @@ public class XYVectorFunction extends XYVector implements XYFunction, InInterval
 	public boolean containsX(double x) {
 		return (size()>0 && x >= getStartX() && x <= getEndX());
 	}
-	public int getPreviousIndex(double x) {
-		if(size()>0) {
-			if(containsX(x)) {
-				if(getStartX()==x) {
-					return 0;
+	public int previousIndex(double x) {
+		if(containsX(x)) {
+			if(getStartX()==x) {
+				return -1;
+			}
+			for(int i=1;i<size();i++) {
+				if(getX(i)>=x) {
+					return i-1;
 				}
-				for(int i=1;i<size();i++) {
-					if(getX(i)>x) {
-						return i-1;
-					}
-				}
-				return size()-1;
-			} 
-		}
+			}
+			if(getEndX()==x && size()>1) {
+				return size()-2;
+			}
+		} 
 		return -1;
+	}
+	public int followingIndex(double x) {
+		if(containsX(x)) {
+			if(getStartX()==x && size()>1) {
+				return 1;
+			}
+			for(int i=0;i<size()-1;i++) {
+				if(getX(i)==x) {
+					return i+1;
+				}
+				if(getX(i)>x) {
+					return i;
+				}
+			}
+			if(getEndX()>x) {
+				return size()-1;
+			}
+			if(getEndX()==x) {
+				return -1;
+			}
+		} 
+		return -1;		
 	}
 	// Interface InInterval
 	@Override
@@ -61,26 +83,69 @@ public class XYVectorFunction extends XYVector implements XYFunction, InInterval
 	}
 
 	// Interface XYFunction
+	/**
+	 * Devuelve el valor de Y para un valor de x comprendido
+	 * en el intervalo. Si la x coincide con una de las de la 
+	 * serie devuelve el valor exacto correspondiente. Si la x
+	 * es intermedia entre dos de la serie devuelve la 
+	 * interpolación lineal de las Y del punto anterior y
+	 * el siguiente
+	 */
 	@Override
 	public double getY(double x) {
-		if(contains(x)) {
-			
-			return get(0)[1];
+		if(containsX(x)) {
+			if(getStartX()==x) {
+				return get(0)[1];
+			}
+			if(getEndX()==x) {
+				return get(size()-1)[1];
+			}
+			int previous = previousIndex(x);
+			int following =followingIndex(x);
+			if(following-previous==1) {
+				// Punto intermedio: Aproximación lineal
+				return (get(previous)[1]+get(following)[1])/2.0;
+			}
+			// Punto exacto
+			return get(previous+1)[1];
 		} else {
 			return Double.NaN;			
 		}
 	}
 
+	/**
+	 * Calcula la tangente según la fórmula:<br/>
+	 * tangent = inc(Y)/inc(X) <br/>
+	 * Si el punto coincide con uno de la serie, excepto el último,
+	 *  el valor es exacto. El último punto no tiene tangente.
+	 *  Si el punto es intermedio, devuelve la interpolación 
+	 *  lineal de la tangente del punto anterior y el siguiente
+	 */
 	@Override
 	public double getTangent(double x) {
-		// TODO Auto-generated method stub
-		return 0;
+		if(containsX(x)) {
+			int following = followingIndex(x);
+			if(following ==-1) {
+				return Double.NaN;
+			}
+			double x1 = x;
+			double y1 = getY(x);
+			double x2 = getX(following);
+			double y2 = getY(following);
+			double incx = x2 - x1;
+			double incy = y2 - y1;
+			return incy/incx;
+		}
+		return Double.NaN;
 	}
 
+	/**
+	 * TODO: No implementado
+	 */
 	@Override
 	public double getCurvature(double x) {
 		// TODO Auto-generated method stub
-		return 0;
+		return Double.NaN;
 	}
 
 }
--- src/test/java/com/mlab/pg/xyfunction/TestXYVectorFunction.java
+++ src/test/java/com/mlab/pg/xyfunction/TestXYVectorFunction.java
@@ -1,7 +1,5 @@
 package com.mlab.pg.xyfunction;
 
-import static org.junit.Assert.*;
-
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.junit.BeforeClass;
@@ -61,6 +59,99 @@ public class TestXYVectorFunction {
 
 	
 		
+	}
+
+	@Test
+	public void testPreviousIndex() {
+		LOG.debug("testPreviousIndex()");
+		XYVectorFunction vector = new XYVectorFunction();
+		vector.add(new double[]{-1.0,1.0});
+		vector.add(new double[]{0.0,1.0});
+		vector.add(new double[]{1.0,1.0});
+		// x no contenido
+		Assert.assertEquals(-1, vector.previousIndex(-2.0));
+		Assert.assertEquals(-1, vector.previousIndex(2.0));
+		// x coincide con el extremo izquierdo
+		Assert.assertEquals(-1, vector.previousIndex(-1.0));
+		// x es un punto intermedio
+		Assert.assertEquals(0, vector.previousIndex(-0.50));
+		Assert.assertEquals(1, vector.previousIndex(0.50));
+		
+		// x coincide con un punto intermedio
+		Assert.assertEquals(0, vector.previousIndex(0.0));
+		// x coincide con el extremo derecho y size()>1
+		Assert.assertEquals(1, vector.previousIndex(1.0));
+
+		// x coincide con el extremo derecho y el tamaño es 1
+		vector = new XYVectorFunction();
+		vector.add(new double[]{1.0,1.0});
+		Assert.assertEquals(-1, vector.previousIndex(1.0));
+
+	}
+	@Test
+	public void testFollowingIndex() {
+		LOG.debug("testFollowingIndex()");
+		XYVectorFunction vector = new XYVectorFunction();
+		vector.add(new double[]{-1.0,1.0});
+		vector.add(new double[]{0.0,1.0});
+		vector.add(new double[]{1.0,1.0});
+		// x no contenido
+		Assert.assertEquals(-1, vector.followingIndex(-2.0));
+		Assert.assertEquals(-1, vector.followingIndex(2.0));
+		// x coincide con el extremo izquierdo
+		Assert.assertEquals(1, vector.followingIndex(-1.0));
+		
+		// x es un punto intermedio
+		Assert.assertEquals(1, vector.followingIndex(-0.5));
+		Assert.assertEquals(2, vector.followingIndex(0.5));
+		
+		// x coincide con un punto intermedio
+		Assert.assertEquals(2, vector.followingIndex(0.0));
+		
+		// x coincide con el extremo derecho
+		Assert.assertEquals(-1, vector.followingIndex(1.0));
+
+		// x coincide con el extremo izquierdo y el tamaño es 1
+		vector = new XYVectorFunction();
+		vector.add(new double[]{1.0,1.0});
+		Assert.assertEquals(-1, vector.followingIndex(1.0));
+
+	}
+	@Test
+	public void testGetY() {
+		LOG.debug("testGetY()");
+		XYVectorFunction vector = new XYVectorFunction();
+		vector.add(new double[]{-1.0,1.0});
+		vector.add(new double[]{0.0,2.0});
+		vector.add(new double[]{1.0,3.0});
+		// x coincide extremo izquierdo
+		Assert.assertEquals(1.0, vector.getY(-1.0), 0.001);
+		// x intermedio entre primer y segundo punto
+		Assert.assertEquals(1.5, vector.getY(-0.5), 0.001);
+		// x coincide con punto intermedio
+		Assert.assertEquals(2.0, vector.getY(0.0), 0.001);
+		// x intermedio entre penúltimo y último punto
+		Assert.assertEquals(2.5, vector.getY(0.5), 0.001);
+		// x coincide con el último punto
+		Assert.assertEquals(3.0, vector.getY(1.0), 0.001);		
+	}
+	@Test
+	public void testGetTangent() {
+		LOG.debug("testGetTangent()");
+		XYVectorFunction vector = new XYVectorFunction();
+		vector.add(new double[]{-1.0,1.0});
+		vector.add(new double[]{0.0,2.0});
+		vector.add(new double[]{1.0,4.0});
+		// x coincide extremo izquierdo
+		Assert.assertEquals(1.0, vector.getTangent(-1.0), 0.001);
+		// x intermedio entre primer y segundo punto
+		Assert.assertEquals(1.0, vector.getTangent(-0.5), 0.001);
+		// x coincide con punto intermedio
+		Assert.assertEquals(2.0, vector.getTangent(0.0), 0.001);
+		// x intermedio entre penúltimo y último punto
+		Assert.assertEquals(2.0, vector.getTangent(0.5), 0.001);
+		// x coincide con el último punto
+		Assert.assertEquals(Double.NaN, vector.getTangent(1.0), 0.001);		
 	}
 
 }
